1b5083059d74f8076e5a21e4192b335d45be57e9
jayukasode23 <jayukasode2398@gmail.com>
2022-10-17 20:15:34 +0530

    Added Fetch Inventary Details

---
+++ FetchInventaryDetails.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventaryManagementProgram
+{
+    public class FetchInventaryDetails
+    {
+        public Rice Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+
+                }
+                catch (Exception)
+                {
+                    return null;
+
+                }
+            }
+        }
+    }
+}
