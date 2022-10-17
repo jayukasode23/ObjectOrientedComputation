7be7e205f9b812e6ca83cca30ea30e9cf5c52389
jayukasode23 <jayukasode2398@gmail.com>
2022-10-17 20:03:31 +0530

    Added Inventary Data

---
+++ Rice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventaryDataManagementProgram
+{
+    public class Rice
+    {
+        public List<TypeofRice> typeofRice;
+        public List<TypeofPulses> typeofPulses;
+        public List<TypeofWheat> typeofWheat;
+
+    }
+}
+public class TypeofRice
+{
+
+    public string name;
+    public int weight;
+    public int price;
+
+}
+public class TypeofPulses
+{
+    public string name;
+    public int weight;
+    public int price;
+}
+
+public class TypeofWheat
+{
+    public string name;
+    public int weight;
+    public int price;
+}
+    
